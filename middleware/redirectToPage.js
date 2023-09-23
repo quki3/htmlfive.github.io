1789de200a4386ff8fff59f4db7998125eae94fa
quki3 <gabrielbrufau@gmail.com>
2023-09-23 20:14:02 -0300

    23-09-23

---
+++ middleware/redirectToPage.js
@@ -0,0 +1,12 @@
+function redirectToPage(s) {
+	if(s=="panel"){
+		window.location.href = "https://mpago.la/121CQom";
+	};	
+	if(s==""){
+		window.location.href = "https://mpago.la/121CQom";
+	};
+	if(s==""){
+		window.location.href = "https://mpago.la/121CQom";
+	};
+
+};
